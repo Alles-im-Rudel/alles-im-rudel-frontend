493cfde8d54dcd43fe3b9c65ca53600072f4ad17
Silas <silas.beckmann@t-online.de>
2021-04-27 07:57:03 +0200

    Items

--- src/components/summoners/summonerInfo/DetailCard.vue
+++ src/components/summoners/summonerInfo/DetailCard.vue
@@ -2,11 +2,72 @@
   <v-card flat v-if="!!summoner.summoner.summonerId">
     <v-card-title>
       {{ summoner.summoner.name }}
+      <v-spacer />
+      <div class="item-row">
+        <v-tooltip top>
+          <template v-slot:activator="{ on }">
+            <v-img
+                max-width="30%"
+                :src="summoner.spell1.image"
+                v-on="on"
+            />
+          </template>
+          <span v-html="summoner.spell1.tooltip" />
+        </v-tooltip>
+        <v-tooltip top>
+          <template v-slot:activator="{ on }">
+            <v-img
+                max-width="30%"
+                :src="summoner.spell2.image"
+                v-on="on"
+            />
+          </template>
+          <span v-html="summoner.spell2.tooltip" />
+        </v-tooltip>
+      </div>
     </v-card-title>
     <v-card-text>
-      <v-row>
 
+      <v-row no-gutters>
+        <v-col cols="12">
+          <div
+              class="item-row"
+          >
+            <v-img
+                class="item"
+                :src="summoner.stats.item0"
+            />
+            <v-img
+                class="item"
+                :src="summoner.stats.item1"
+            />
+            <v-img
+                class="item"
+                :src="summoner.stats.item2"
+            />
+            <v-img
+                class="item"
+                :src="summoner.stats.item3"
+            />
+            <v-img
+                class="item"
+                :src="summoner.stats.item4"
+            />
+            <v-img
+                class="item"
+                :src="summoner.stats.item5"
+            />
+            <v-img
+                class="item"
+                :src="summoner.stats.item6"
+            />
+          </div>
+        </v-col>
+        <v-col cols="12">
+
+        </v-col>
       </v-row>
+
     </v-card-text>
   </v-card>
   <v-card flat v-else>
@@ -31,3 +92,18 @@ export default {
   methods: {}
 }
 </script>
+
+<style>
+.item {
+  border-color: goldenrod;
+  border-style: solid;
+  border-width: 1px;
+  min-width: 15%;
+  max-width: 15%;
+}
+
+.item-row {
+  display: flex;
+  align-items: stretch;
+}
+</style>
