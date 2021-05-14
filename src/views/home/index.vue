e6c1082e08bccbcfd4b1e5c89575e2ee24d7c20b
Silas <silas.beckmann@t-online.de>
2021-05-14 19:32:35 +0200

    WIP

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,45 +1,21 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="12" md="12">
-        <base-card>
-          <v-card-title>
-            {{ homeView.title }}
-            <v-spacer />
-            <view-edit-button :view-id="1" :label="homeView.title+' bearbeiten'" @reload="getHomeView" />
-          </v-card-title>
-          <v-divider />
-          <div class="ql-snow">
-            <div class="ql-editor" v-html="homeView.body" />
-          </div>
-          <vue-editor style="display:none"/>
-        </base-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-parallax src="../../../assets/backgraound.jpg">
+    <h1>Alles Im Rudel</h1>
+  </v-parallax>
 </template>
 <script>
-import ViewEditButton from "@/components/views/ViewEditButton";
-import {mapActions, mapGetters} from 'vuex';
 
 export default {
   name: 'Home',
-  components: {
-    ViewEditButton
-  },
+  components: {},
   data() {
     return {
       isLoading: true
     };
   },
-  computed: {
-    ...mapGetters('view', ['homeView'])
-  },
+  computed: {},
   created() {
-    this.getHomeView();
   },
-  methods: {
-    ...mapActions('view', ['getHomeView']),
-  }
+  methods: {}
 };
 </script>
