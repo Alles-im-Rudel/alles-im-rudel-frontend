16e0b91c7bd36857c734551461a14b52ba531cd5
Silas <silas.beckmann@t-online.de>
2021-10-20 13:29:06 +0200

    typo....

--- src/views/joins/parts/ArticalOfAssociation.vue
+++ src/views/joins/parts/ArticalOfAssociation.vue
@@ -25,7 +25,7 @@ export default {
   },
   data() {
     return {
-      src: pdf.createLoadingTask('/pdf/verseionsatzung.pdf'),
+      src: pdf.createLoadingTask('/pdf/vereinssatzung.pdf'),
       numPages: null,
       isLoading: true
     }
