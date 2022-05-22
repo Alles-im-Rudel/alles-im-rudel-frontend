fc17269a07286388e09152b46bcb2ada1065bf14
vollborn <oliver.vollborn@dreid.de>
2022-05-22 22:59:48 +0200

    Ignore: WIP

--- src/views/sponsoring/index.vue
+++ src/views/sponsoring/index.vue
@@ -1,35 +1,144 @@
 <template>
   <div class="white">
-    <BaseContainer>
-      <v-row
-        class="mt-6"
-        align="center"
-      >
-        <v-col cols="12">
-          <h2 class="text-h5 mb-2">
-            Unsere Partner
-          </h2>
-        </v-col>
-        <v-col>
-          <a
-            href="https://www.autoreparatur-krohn.de"
-            target="_blank"
-          >Autoreparatur Henry Krohn</a>
-          <p>
-            10% Rabatt auf alle Reparaturen und Dienstleistungen
-          </p>
-          <a
-            href="https://www.begadi.com"
-            target="_blank"
-          >Begadi</a>
+    <BaseSectionTitle>
+      Sponsoring
+    </BaseSectionTitle>
 
-          <a
-            href="https://badagency.de/"
-            target="_blank"
-          > Bad Agency</a>
-        </v-col>
-      </v-row>
-      <v-divider class="mt-3 mb-10" />
+    <BaseContainer class="mt-14 mb-14">
+      <BaseParagraph class="mb-12">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+        kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+        sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+        sanctus est Lorem ipsum dolor sit amet.
+      </BaseParagraph>
+
+      <v-expansion-panels>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            <v-row class="expansion-panel-logo-container">
+              <v-col
+                cols="9"
+                sm="3"
+                lg="2"
+              >
+                <img
+                  src="/assets/sponsors/henry-krohn/henry-krohn-quadratic.jpg"
+                  alt="Henry Krohn Logo"
+                >
+              </v-col>
+              <v-col
+                cols="12"
+                sm="9"
+                lg="10"
+                class="d-flex align-center"
+              >
+                <div>
+                  <h2>Autoreparatur Henry Krohn</h2>
+                  <SponsoringAvailableLabel />
+                </div>
+              </v-col>
+            </v-row>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            10% Rabatt auf alle Reparaturen und Dienstleistungen
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+
+      <v-expansion-panels class="mt-6">
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            <v-row class="expansion-panel-logo-container">
+              <v-col
+                cols="9"
+                sm="3"
+                lg="2"
+              >
+                <img
+                  src="/assets/sponsors/begadi/begadi.png"
+                  alt="Begadi"
+                >
+              </v-col>
+              <v-col
+                cols="12"
+                sm="9"
+                lg="10"
+                class="d-flex align-center"
+              >
+                <div>
+                  <h2>Begadi</h2>
+                  <SponsoringAvailableLabel
+                    :available="hasAirsoft"
+                    branch="Airsoft"
+                  />
+                </div>
+              </v-col>
+            </v-row>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            lalelu
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+
+      <v-expansion-panels class="mt-6">
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            <v-row class="expansion-panel-logo-container">
+              <v-col
+                cols="9"
+                sm="3"
+                lg="2"
+              >
+                <img
+                  src="/assets/sponsors/bad-agency/bad-agency.png"
+                  alt="Bad Agency"
+                >
+              </v-col>
+              <v-col
+                cols="12"
+                sm="9"
+                lg="10"
+                class="d-flex align-center"
+              >
+                <h2>Bad Agency</h2>
+                <SponsoringAvailableLabel
+                  :available="hasAirsoft"
+                  branch="Airsoft"
+                />
+              </v-col>
+            </v-row>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            lalelu
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </BaseContainer>
   </div>
 </template>
+
+<script>
+import SponsoringAvailableLabel from '@/views/sponsoring/parts/SponsoringAvailableLabel';
+import {mapGetters} from 'vuex';
+
+export default {
+  components: {SponsoringAvailableLabel},
+  computed: {
+    ...mapGetters('auth', ['user']),
+    hasAirsoft() {
+      return false;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.expansion-panel-logo-container {
+  img {
+    width: 100%;
+  }
+}
+</style>
---
+++ src/views/sponsoring/parts/SponsoringAvailableLabel.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="d-flex align-center mt-3">
+    <v-icon
+      class="mr-2"
+      :color="available ? 'success' : 'error'"
+      small
+    >
+      {{ available ? 'fa-check' : 'fa-times' }}
+    </v-icon>
+    {{
+      available
+        ? 'Dieser Sponsor ist für dich verfügbar!'
+        : 'Dieser Sponsor ist nur für die Sparte ' + branch + ' verfügbar.'
+    }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    available: {
+      type: Boolean,
+      required: false,
+      default: true
+    },
+    branch: {
+      type: String,
+      required: false,
+      default: null
+    }
+  }
+};
+</script>
