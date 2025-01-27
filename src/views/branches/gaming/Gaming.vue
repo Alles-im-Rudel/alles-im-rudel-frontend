e84bc6ac5b548a934fde0fb4c6061e2810203098
Ulli Weichert <ullrich@weichert.it>
2025-01-27 22:57:53 +0100

    changed format

--- src/views/branches/gaming/Gaming.vue
+++ src/views/branches/gaming/Gaming.vue
@@ -17,14 +17,32 @@
             Moin!
           </h2>
           <BaseParagraph class="mt-6 mt-sm-8">
-          Auch unter der ziemlich jungen Gamingsparte finden sich ein paar coole Leute, die sich aktuell hauptsächlich in den Welten von Rocket League und League of Legends austobten, jedoch würden nicht vor anderen Games zurückschrecken. Bei uns steht der Spaß immer an erster Stelle, egal ob wir entspannt zocken oder uns in der kompetitiven Szene beweisen wollen. Wir suchen immer nach neuen Mitspielern, die nicht nur freundlich, sondern auch super gesellig sind. 
-Natürlich sind aber auch alle herzlich willkommen, die einfach nur in ihrer Freizeit ein bisschen zocken wollen und auf der Suche nach coolen Mates sind. Wenn ihr mehr über uns erfahren oder uns persönlich kennenlernen wollt, dann schaut doch einfach mal auf unserem Discord-Server vorbei. Lasst uns gemeinsam die Gaming-Welt rocken!
-Was erwartet dich in der Gamingsparte?
+          Auch unter der ziemlich jungen Gamingsparte finden sich ein paar coole Leute, 
+          die sich aktuell hauptsächlich in den Welten von Rocket League und League of Legends 
+          austobten, jedoch würden nicht vor anderen Games zurückschrecken. 
+          Bei uns steht der Spaß immer an erster Stelle, egal ob wir entspannt zocken oder uns 
+          in der kompetitiven Szene beweisen wollen. Wir suchen immer nach neuen Mitspielern, 
+          die nicht nur freundlich, sondern auch super gesellig sind. 
+          
+          Natürlich sind aber auch alle herzlich willkommen, die einfach nur in ihrer Freizeit 
+          ein bisschen zocken wollen und auf der Suche nach coolen Mates sind. Wenn ihr mehr 
+          über uns erfahren oder uns persönlich kennenlernen wollt, dann schaut doch einfach 
+          mal auf unserem Discord-Server vorbei. Lasst uns gemeinsam die Gaming-Welt rocken!
+          
+          Was erwartet dich in der Gamingsparte?
 
-            Die Gamingsparte ist noch ziemlich jung und im Aufbau. Daher sind wir aktiv auf der Suche nach Leuten, die Interesse haben im Rahmen eines E-Sport Teams für Alles im Rudel kompetitiv solo oder im Team je nach Game zu beweisen. Ebenfalls finden gelegentlich diverse Events intern statt wie z.B. die  "Game Nights" für einen entspannten Abend gemeinsam auf <a
+          Die Gamingsparte ist noch ziemlich jung und im Aufbau. Daher sind wir aktiv auf 
+          der Suche nach Leuten, die Interesse haben im Rahmen eines E-Sport Teams für Alles 
+          im Rudel kompetitiv solo oder im Team je nach Game zu beweisen. Ebenfalls finden 
+          gelegentlich diverse Events intern statt wie z.B. die  "Game Nights" für einen 
+          entspannten Abend gemeinsam auf <a
               :href="discordUrl"
               target="_blank"
-            >Discord</a> oder mehr kompetitive Events wie den "Alles im Rudel Beta Cup", in dieser sich die Vereinsmitglieder untereinander in League of Legends duellierten. Auch steht ein Projekt im Bereich Pen & Paper in Planung
+            >Discord</a> oder mehr
+            kompetitive Events wie den "Alles im Rudel Beta Cup", in dieser sich die Vereinsmitglieder 
+            untereinander in League of Legends duellierten. 
+            
+            Auch steht ein Projekt im Bereich Pen & Paper in Planung.
           </BaseParagraph>
         </v-col>
 
